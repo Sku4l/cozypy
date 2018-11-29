40b246fb9eb277f67a2680bf140cd69eec5091fc
Marc PEPIN <m.pepin@deadia.fr>
2018-11-29 15:50:12 +0100

    Add device type enum

--- cozypy/constant.py
+++ cozypy/constant.py
@@ -12,5 +12,17 @@ class DeviceType(enum.Enum):
     OCCUPANCY = "OccupancySensor"
     ELECTRECITY = "CumulativeElectricPowerConsumptionSensor"
 
+
+class DeviceStateType(enum.Enum):
+    INT = 1
+    FLOAT = 2
+    STR = 3
+    DICT = 11
+    LIST = 10
+
+
 class DeviceState(enum.Enum):
-    TEMPERATURE_STATE = "core:TemperatureState"
+    CURRENT_TEMPERATURE_STATE = "core:TemperatureState"
+    COMFORT_TEMPERATURE_STATE = "core:ComfortRoomTemperatureState"
+    ECO_TEMPERATURE_STATE = "core:EcoRoomTemperatureState"
+    ON_OFF_STATE = "core:OnOffState"
