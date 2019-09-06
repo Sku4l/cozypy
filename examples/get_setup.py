e372320bfb4f98d1ea3cceac16ac7bfbb8561fd1
Unknown <biker91620@gmail.com>
2019-09-06 03:03:53 +0200

    update examples

--- examples/get_setup.py
+++ examples/get_setup.py
@@ -21,14 +21,22 @@ try:
     for heater in setup.heaters:
         print("\t", heater.name)
         print("\t\t", heater.id)
+        print("\t\t", heater.place.name)
         print("\t\t", heater.supported_states)
+        print("\t\t", heater.is_on)
+        print("\t\t", heater.operating_mode)
         for state in heater.supported_states:
             print("\t\t", state.value, heater.get_state(state))
 
+        if heater.id == "7b56551a-badd-4dae-b12a-7a5864d8e6b5":
+            heater.set_targeting_heating_level("comfort")
+            heater.update()
         if len(heater.sensors) > 0:
             print("\t\tSensors")
             for sensor in heater.sensors:
-                print("\t\t\t %s %s %s" % (sensor.id, sensor.name, sensor.widget))
+                print("\t\t\t {id} {name} {type}".format(id=sensor.id, name=sensor.name, type=sensor.widget))
+                for sensor_state in sensor.states:
+                    print("\t\t\t\t {name}: {value}".format(name=sensor_state["name"], value=sensor_state["value"]))
 
 
 
