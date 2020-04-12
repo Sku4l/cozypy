21e2b1818bff56a29765a0d68c7c7c2930bc2c92
cyr-ius <cyr-ius@ipocus.net>
2020-04-12 17:31:55 +0200

    Add import exceptions

--- cozytouchpy/__init__.py
+++ cozytouchpy/__init__.py
@@ -3,7 +3,8 @@
 Provides authentification and row access to Cozytouch modules.
 '''
 name = 'cozytouchpy'
-__version__ = '1.4'
+__version__ = '1.4.1'
 __all__ = ['cozytouchpy']
 
 from .client import CozytouchClient
+from .exception import CozytouchException
