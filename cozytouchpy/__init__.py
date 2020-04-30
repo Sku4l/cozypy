e9c3992e722796568b291abcd48a7fbf6218c150
cyr-ius <cyr-ius@ipocus.net>
2020-04-30 16:57:55 +0200

    Update to 1.5.5

--- cozytouchpy/__init__.py
+++ cozytouchpy/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 """Provides authentification and row access to Cozytouch modules."""
 name = "cozytouchpy"
-__version__ = "1.5.4"
+__version__ = "1.5.5"
 
 from .client import CozytouchClient
 from .exception import CozytouchException, CozytouchAuthentificationFailed
