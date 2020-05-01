c3c0319e18e79084aedc5cb13e5ea66ea14a97d4
cyr-ius <cyr-ius@ipocus.net>
2020-05-01 12:35:23 +0200

    Bump to 1.5.9

--- cozytouchpy/__init__.py
+++ cozytouchpy/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 """Provides authentification and row access to Cozytouch modules."""
 name = "cozytouchpy"
-__version__ = "1.5.8"
+__version__ = "1.5.9"
 
 from .client import CozytouchClient
 from .exception import CozytouchException, AuthentificationFailed, HttpRequestFailed
