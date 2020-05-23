fa4590a7c019447c67d66a21aa81ba24684e774d
cyr-ius <cyr-ius@ipocus.net>
2020-05-23 11:12:41 +0200

    Bump to 1.6.6

--- cozytouchpy/__init__.py
+++ cozytouchpy/__init__.py
@@ -4,7 +4,7 @@ from .client import CozytouchClient
 from .exception import AuthentificationFailed, CozytouchException, HttpRequestFailed
 
 name = "cozytouchpy"
-__version__ = "1.6.5d"
+__version__ = "1.6.6"
 __all__ = [
     "CozytouchClient",
     "AuthentificationFailed",
